4f07278105db53c5bfe1c49593d88aa489727d93
DESKTOP-2GN0AV4\Mihaela <mihaelarot3@gmail.com>
2023-12-12 22:27:01 +0200

    firebase recycleview done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.adapter.TicketAdapter
 import com.example.myapplication.classes.Ticket
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 
 class MainActivity : AppCompatActivity() {
 
     private var ticketList: ArrayList<Ticket>? = ArrayList()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,15 +25,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getInformation()
-        //recycleview_dashboard_list_user.adapter?.notifyItemChanged(TransactionManager.getTransactionsList().size)
+        
+    }
 
-
+    private fun getDataFirebase() {
+        database = FirebaseFirestore.getInstance()
+        database.collection("tickets").addSnapshotListener(object : EventListener<QuerySnapshot> {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null) {
+                    Log.e("Firestore error", error.message.toString())
+                    return
+                }
+                for (dc: DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
+                        ticketList?.add(dc.document.toObject(Ticket::class.java))
+                    }
+                }
+                initAdapter()
+            }
+        }
+        )
     }
 
     private fun getInformation() {
 
-        ticketList?.add(Ticket("fdsdafds22222", "fdsfds", "dsfdsf"))
-        ticketList?.add(Ticket("fdsdafds", "fdsfds", "dsfdsf"))
+//        ticketList?.add(Ticket("fdsdafds22222", "fdsfds", "dsfdsf"))
+//        ticketList?.add(Ticket("fdsdafds", "fdsfds", "dsfdsf"))
+        getDataFirebase()
         initAdapter()
     }
 
@@ -34,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.recycleviewTicketItem.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter= ticketList?.let { TicketAdapter(it) }
+            adapter = ticketList?.let { TicketAdapter(it) }
         }
     }
 }
