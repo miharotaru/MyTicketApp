be0badc025aaa6b27f49529c492ca4c0acce624e
DESKTOP-2GN0AV4\Mihaela <mihaelarot3@gmail.com>
2023-12-12 15:06:25 +0200

    display list on recycleview

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.adapter.TicketAdapter
+import com.example.myapplication.classes.Ticket
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var ticketList: ArrayList<Ticket>? = ArrayList()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getInformation()
+        //recycleview_dashboard_list_user.adapter?.notifyItemChanged(TransactionManager.getTransactionsList().size)
+
+
+    }
+
+    private fun getInformation() {
+
+        ticketList?.add(Ticket("fdsdafds22222", "fdsfds", "dsfdsf"))
+        ticketList?.add(Ticket("fdsdafds", "fdsfds", "dsfdsf"))
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+
+        binding.recycleviewTicketItem.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter= ticketList?.let { TicketAdapter(it) }
+        }
     }
 }
