47e0f777b1c410ec6012c40d9f97864cb8a7e708
DESKTOP-2GN0AV4\Mihaela <mihaelarot3@gmail.com>
2024-02-29 15:48:42 +0200

    implemented PaymentDetailsTicketFragment

--- MyApplication/app/src/main/java/com/example/myapplication/classes/Ticket.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/classes/Ticket.kt
@@ -6,8 +6,13 @@ import java.io.Serializable
 data class Ticket (
     var title: String="",
     var location: String="",
+    var city:String="",
     var data: String="",
     var urlToImage: String="",
     var details:String="",
-    //var numberTickets:Int
+    var numberTickets:Int=-1,
+    var priceCategoryOne: Int=-1,
+    var priceCategoryTwo: Int=-1,
+    var priceCategoryThree: Int=-1,
+    var priceCategoryVIP: Int=-1,
 )
--- MyApplication/app/src/main/java/com/example/myapplication/fragments/PaymentDetailsTicketFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/fragments/PaymentDetailsTicketFragment.kt
@@ -1,22 +1,24 @@
 package com.example.myapplication.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
 import android.widget.Toast
 import com.example.myapplication.R
 import com.example.myapplication.classes.Ticket
-import com.example.myapplication.databinding.FragmentDetailsTicketBinding
 import com.example.myapplication.databinding.FragmentPaymentDetailsTicketBinding
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
-import com.squareup.picasso.Picasso
 
 
 class PaymentDetailsTicketFragment : Fragment() {
@@ -25,11 +27,14 @@ private lateinit var binding: FragmentPaymentDetailsTicketBinding
     private var ticketPos: Int = -1
     var ticketList: ArrayList<Ticket>? = ArrayList()
     private lateinit var database: FirebaseFirestore
+    private lateinit var ticketItem: Ticket
+    private lateinit var alertDialog: AlertDialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding=FragmentPaymentDetailsTicketBinding.inflate(inflater, container,false)
+        alertDialog = AlertDialog.Builder(requireContext()).create()
         return binding.root
     }
 
@@ -40,8 +45,83 @@ private lateinit var binding: FragmentPaymentDetailsTicketBinding
             ticketPos =
                 it.getInt("ticketItem", -1) // -1 este valoarea implicită dacă nu se găsește cheia
         }
+
+        setButtonsForNumberOfTicket()
+        setSumOfTicketRadioButton()
+
     }
 
+    private fun setSumOfTicketRadioButton() {
+        binding.radioGroupLeft.setOnCheckedChangeListener { group, checkedId ->
+            val selectedRadioButton = binding.root.findViewById<RadioButton>(checkedId)
+            val selectedNumber = selectedRadioButton.text.toString().toInt()
+            val currentNumberOfTickets = binding.tvShowNumberOfTickets.text.toString().toInt()
+            val totalSum = selectedNumber * currentNumberOfTickets
+            binding.tvSumaTotalaTicket.text = totalSum.toString()
+        }
+    }
+
+
+    private fun setButtonsForNumberOfTicket() {
+        binding.btPlusTicket.setOnClickListener {
+            val currentNumberOfTickets = binding.tvShowNumberOfTickets.text.toString().toInt()
+            if (currentNumberOfTickets < 10) {
+                val newNumberOfTickets = currentNumberOfTickets + 1
+                binding.tvShowNumberOfTickets.text = newNumberOfTickets.toString()
+            }
+            updateButtonStates()
+        }
+
+        binding.btMinusTicket.setOnClickListener {
+            val currentNumberOfTickets = binding.tvShowNumberOfTickets.text.toString().toInt()
+            if (currentNumberOfTickets > 1) {
+                val newNumberOfTickets = currentNumberOfTickets - 1
+                binding.tvShowNumberOfTickets.text = newNumberOfTickets.toString()
+            }
+            updateButtonStates()
+        }
+    }
+
+    private fun updateButtonStates() {
+
+
+        val currentNumberOfTickets = binding.tvShowNumberOfTickets.text.toString().toInt()
+        binding.btPlusTicket.isEnabled = currentNumberOfTickets < 10
+        binding.btMinusTicket.isEnabled = currentNumberOfTickets > 1
+        if(currentNumberOfTickets >= ticketItem.numberTickets){
+            binding.btPlusTicket.isEnabled=currentNumberOfTickets < ticketItem.numberTickets
+           // Toast.makeText(context,"Numar insuficient de bilete",Toast.LENGTH_LONG).show()
+
+            showInsufficientTicketsDialog()
+        }
+
+
+        //fac update la text cand se schimba valoarea nr de tickete
+        val selectedRadioButtonId= binding.radioGroupLeft.checkedRadioButtonId
+        if (selectedRadioButtonId != -1) {
+            val selectedRadioButton = binding.root.findViewById<RadioButton>(selectedRadioButtonId)
+            val selectedNumber = selectedRadioButton.text.toString().toInt()
+            val totalSum = selectedNumber * currentNumberOfTickets
+            binding.tvSumaTotalaTicket.text = totalSum.toString()
+        }
+    }
+    private fun showInsufficientTicketsDialog() {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog, null)
+        val dialogMessage = dialogView.findViewById<TextView>(R.id.dialog_message)
+        dialogMessage.text = "Numar insuficient de bilete"
+
+        val okButton = dialogView.findViewById<Button>(R.id.ok_button)
+        okButton.setOnClickListener {
+           alertDialog.dismiss()
+        }
+
+        val builder = AlertDialog.Builder(context)
+        builder.setView(dialogView)
+        builder.setCancelable(false)
+
+        alertDialog = builder.create()
+        alertDialog.show()
+    }
     private fun getDataFirebase() {
         database = FirebaseFirestore.getInstance()
         database.collection("tickets").addSnapshotListener(object : EventListener<QuerySnapshot> {
@@ -68,14 +148,21 @@ private lateinit var binding: FragmentPaymentDetailsTicketBinding
         if (ticketPos != -1) {
             Log.d("Fragment_ticketItem", ticketList.toString())
             ticketList?.get(ticketPos)?.let { ticket ->
+                //
+                ticketItem=ticket
+
                 // Accesezi TextView-urile din binding și setezi textul
-//                binding.tvTitleDetailsTicket.text = ticket.title
-//                binding.tvTitleTicketDetailsTicketFromTicket.text = ticket.title
-//                binding.tvTextDetailsTicket.text = ticket.details
-//                Picasso.get().load(ticket.urlToImage).into(binding.imageDetailsTicket)
+                binding.tvNameTicketPayment.text = ticket.title
+                binding.tvLocationTicketPayment.text = ticket.location
+                binding.tvDateTicketPayment.text = ticket.data
+                binding.tvShowNumberOfTickets.text = "1"
+                binding.tvSumaTotalaTicket.text="-"
+
+                binding.radioButton1.text = ticket.priceCategoryOne.toString()
+                binding.radioButton2.text = ticket.priceCategoryTwo.toString()
+                binding.radioButton3.text = ticket.priceCategoryThree.toString()
+                binding.radioButton4.text = ticket.priceCategoryVIP.toString()
             }
-
-
 
         } else {
             Log.d("Fragment_ticketItem", "Nu s-a transmis corect argumentul")
